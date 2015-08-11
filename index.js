1f5b582d757f334f1b6f524191dfd0772ffd2877
emensch <eli.mensch@gmail.com>
2015-08-11 16:14:20 -0400

    Add basic model scaffolding

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ db.connect('database/app.db', function (err) {
 	}
 });
 
+var Artist = require('./models/artist');
+var Employee = require('./models/employee');
+
 var server = app.listen(3000, function () {
 	var port = server.address().port;
 	console.log('Listening on port %s...', port);
