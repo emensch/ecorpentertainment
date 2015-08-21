b92a27bd5d377dcd55419216ab85fb09e25bd0ca
emensch <eli.mensch@gmail.com>
2015-08-20 19:24:50 -0500

    Test post route and bodyParser

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express'),
+	bodyParser = require('body-parser'),
 	db = require('./db');
 
 var app = express();
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded( { extended: false } ));
 
 // Must open database connection before requiring models
 db.connect('database/app.db', function (err) {
@@ -19,6 +21,15 @@ app.get('/', function (req, res) {
 	Artist.findAll( function (err, artists) {
 		res.json(artists);
 	});
+});
+
+app.get('/admin', function (req, res) {
+	res.render('admin.jade')
+});
+
+app.post('/admin', function (req, res) {
+	console.log(req.body);
+	res.sendStatus(200);
 });
 
 var server = app.listen(3000, function () {
