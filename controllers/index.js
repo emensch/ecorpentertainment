acc441ded81df4e1099bca94ce242ca706c31766
emensch <eli.mensch@gmail.com>
2015-08-22 13:03:51 -0400

    Scaffold templates/routes for artist/employee managing

--- controllers/index.js
+++ controllers/index.js
@@ -17,6 +17,34 @@ router.get('/admin', function (req, res) {
 	res.render('admin.jade')
 });
 
+router.get('/admin/editartist', function (req, res) {
+	res.render('manage_artist.jade', {
+		method: 'PUT',
+		title: 'Edit artist'
+	});
+});
+
+router.get('/admin/newartist', function (req, res) {
+	res.render('manage_artist.jade', {
+		method: 'POST',
+		title: 'New artist'
+	});
+});
+
+router.get('/admin/editemployee', function (req, res) {
+	res.render('manage_employee.jade', {
+		method: 'PUT',
+		title: 'Edit employee'
+	});
+});
+
+router.get('/admin/newemployee', function (req, res) {
+	res.render('manage_employee.jade', {
+		method: 'POST',
+		title: 'New employee'
+	});
+});
+
 router.post('/admin', function (req, res) {
 	console.log(req.body);
 	res.sendStatus(200);
