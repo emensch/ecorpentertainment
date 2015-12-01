710b2f2d9cadc6a3f624a3185b6c93a0834ed5cd
emensch <eli.mensch@gmail.com>
2015-11-30 22:10:36 -0500

    Add basic js files for one-page

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+(function() {
+    'use strict';
+
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            menu: '#menu',
+            onLeave: onLeave,
+            afterLoad: afterLoad
+        });
+
+        $('#messagetext').focus(function() {
+            console.log('hasfocus');
+        });
+    });
+
+    function onLeave(index, nextIndex, direction) {
+        var nav = $('#nav');
+        var className = 'scrolled';
+
+        if(nextIndex !== 1) {
+            nav.addClass(className);
+        } else {
+            nav.removeClass(className);
+        }
+    }
+
+    function afterLoad(anchorLink, index) {
+
+    }
+})();
